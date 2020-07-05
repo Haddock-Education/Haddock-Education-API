01d2aedcdaa93115047f3f90f1014202bf73fb20
hugopassos <hugo.passos@hotmail.com>
2020-07-05 19:37:11 -0300

    Created Rankings Controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::UsersController < ApplicationController
-  def index
-    @users = User.all
-    render json: { users: @users }
+  def show
+    user = User.find_by(id: params[:id])
+
+    if user
+      json_response 'Detalhes do usuário', true, { user: user }, :ok
+    else
+      json_response 'Usuário não encontrado', false, {}, :not_found
+    end
   end
 end
